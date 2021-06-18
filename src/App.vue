0924c788766657903433a5a02c186ada20fb3d70
aungmyat12 <aungmyatoo2580@gmail.com>
2021-06-18 22:12:56 +0630

    1-ProjectSetup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <TheHeader />
+  <div class="container">
+    <CalculateTransaction />
+    <TransactionsList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import AddTransaction from "./components/AddTransaction.vue";
+import CalculateTransaction from "./components/CalculateTransaction.vue";
+import TheHeader from "./components/Layouts/TheHeader.vue";
+import TransactionsList from "./components/TransactionsList.vue";
+export default {
+  components: {
+    TheHeader,
+    CalculateTransaction,
+    TransactionsList,
+    AddTransaction,
+  },
+  name: "App",
+};
+</script>
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,38 @@
+<template>
+  <h3>Add new transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Text</label>
+      <input type="text" id="text" placeholder="Enter text..." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount <br />
+        (negative - expense, positive - income)</label
+      >
+      <input type="number" id="amount" placeholder="Enter amount..." />
+    </div>
+    <button class="btn">Add transaction</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+label {
+  display: inline-block;
+  margin: 10px 0;
+}
+
+input[type="text"],
+input[type="number"] {
+  border: 1px solid #dedede;
+  border-radius: 2px;
+  display: block;
+  font-size: 16px;
+  padding: 10px;
+  width: 100%;
+}
+</style>
